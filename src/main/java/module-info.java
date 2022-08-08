72486d5bf3113470486f9ffa7195831d7bedd269
Anastasia Danchenko <nastiadanchenko@gmail.com>
2022-08-08 17:55:59 +0500

    Update openFileView.fxml

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.nastiadanchtnko.test_app_for_mikhail {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.nastiadanchtnko to javafx.fxml;
+    exports com.nastiadanchtnko;
+}
